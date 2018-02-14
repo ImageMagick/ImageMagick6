15d91f5ab1c24d5ae36515a00a9a8b016f53de95
Cristy <urban-warrior@imagemagick.org>
2018-02-14 16:48:28 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6350

--- coders/pcx.c
+++ coders/pcx.c
@@ -361,6 +361,7 @@ static Image *ReadPCXImage(const ImageInfo *image_info,ExceptionInfo *exception)
     if (status == MagickFalse)
       ThrowPCXException(image->exception.severity,image->exception.reason);
     (void) SetImageBackgroundColor(image);
+    (void) memset(pcx_colormap,0,sizeof(pcx_colormap));
     count=ReadBlob(image,3*image->colors,pcx_colormap);
     if (count != (ssize_t) (3*image->colors))
       ThrowPCXException(CorruptImageError,"ImproperImageHeader");
