94d9e5e4c5a7b3b45720ea8c489e2fbbe7c5974b
Cristy <urban-warrior@imagemagick.org>
2018-03-07 18:10:02 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6780

--- coders/wpg.c
+++ coders/wpg.c
@@ -576,6 +576,7 @@ static int UnpackWPG2Raster(Image *image,int bpp)
     sizeof(*BImgBuff));
   if(BImgBuff==NULL)
     return(-2);
+  (void) ResetMagickMemory(BImgBuff,0,ldblk*sizeof(*BImgBuff));
 
   while( y< image->rows)
     {
