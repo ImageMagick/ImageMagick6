d38c8299887e184d35c500c0d59c56488fba25d2
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-20 00:07:18 +0100

    Moved call to DestroyBlob (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6859).

--- coders/wpg.c
+++ coders/wpg.c
@@ -844,11 +844,10 @@ static Image *ExtractPostscript(Image *image,const ImageInfo *image_info,
       (void) CopyMagickString(p->magick_filename,image->magick_filename,
         MagickPathExtent);
       (void) CopyMagickString(p->magick,image->magick,MagickPathExtent);
-      DestroyBlob(p);
       if ((p->rows == 0) || (p->columns == 0))
         {
           DeleteImageFromList(&p);
-          if (p == (Image *) NULL) 
+          if (p == (Image *) NULL)
           {
             image2=(Image *) NULL;
             goto FINISH_UNL;
@@ -856,6 +855,7 @@ static Image *ExtractPostscript(Image *image,const ImageInfo *image_info,
         }
       else
         {
+          DestroyBlob(p);
           p->blob=ReferenceBlob(image->blob);
           p=p->next;
         }
