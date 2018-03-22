0e2e49cca6bd2fd045097a97f03e2ed71529484b
Cristy <urban-warrior@imagemagick.org>
2018-03-21 20:58:25 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5802

--- coders/sct.c
+++ coders/sct.c
@@ -286,13 +286,15 @@ static Image *ReadSCTImage(const ImageInfo *image_info,ExceptionInfo *exception)
         }
         q++;
       }
-      if (c == EOF)
+      if (x < (ssize_t) image->columns)
         break;
       if (SyncAuthenticPixels(image,exception) == MagickFalse)
         break;
       if ((image->columns % 2) != 0)
         (void) ReadBlobByte(image);  /* pad */
     }
+    if (i < (ssize_t) separations)
+      break;
     status=SetImageProgress(image,LoadImageTag,(MagickOffsetType) y,
       image->rows);
     if (status == MagickFalse)
