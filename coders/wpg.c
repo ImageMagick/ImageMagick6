f104e2bc5741b33406be88a2dc167b55081be014
Cristy <urban-warrior@imagemagick.org>
2018-03-31 19:28:23 -0400

    ...

--- coders/wpg.c
+++ coders/wpg.c
@@ -1429,8 +1429,7 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
                       ldblk+1,sizeof(*BImgBuff));
                     if (BImgBuff == (unsigned char *) NULL)
                       goto NoMemory;
-
-                    for (i=0; i< (ssize_t) image->rows; i++)
+                    for (i=0; i < (ssize_t) image->rows; i++)
                     {
                       ssize_t
                         count;
@@ -1439,15 +1438,11 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
                       if (count != ldblk)
                         break;
                       if (InsertRow(BImgBuff,i,image,bpp) == MagickFalse)
-                        {
-                          if(BImgBuff)
-                            BImgBuff=(unsigned char *) RelinquishMagickMemory(BImgBuff);
-                          goto DecompressionFailed;
-                        }
+                        break;
                     }
-
-                    if(BImgBuff)
-                      BImgBuff=(unsigned char *) RelinquishMagickMemory(BImgBuff);
+                    BImgBuff=(unsigned char *) RelinquishMagickMemory(BImgBuff);
+                    if (i < (ssize_t) image->rows)
+                      goto DecompressionFailed;
                     break;
                   }
                 case 1:    /*RLE for WPG2 */
