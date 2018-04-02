3408bf3582e9d865c57e6299d3b0197ce3978f12
Cristy <urban-warrior@imagemagick.org>
2018-04-02 19:28:30 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7313

--- coders/heic.c
+++ coders/heic.c
@@ -413,11 +413,11 @@ static MagickBooleanType ParseIpcoAtom(Image *image, DataBuffer *db,
       ThrowAndReturn("insufficient data");
     if (prop->data != (uint8_t *) NULL)
       prop->data=(uint8_t *) RelinquishMagickMemory(prop->data);
-    prop->data = (uint8_t *) AcquireCriticalMemory(prop->size);
+    prop->data = (uint8_t *) AcquireCriticalMemory(prop->size+4);
     if (DBChop(&propDb, db, prop->size) != MagickTrue) {
       ThrowAndReturn("incorrect read size");
     }
-    memcpy(prop->data, propDb.data, prop->size);
+    memcpy(prop->data, propDb.data, prop->size+4);
 
     switch (prop->type) {
       case ATOM('h', 'v', 'c', 'C'):
