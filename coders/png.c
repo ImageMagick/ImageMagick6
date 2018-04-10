6a6f740e1eab61f40db8e425b8c58904b0155916
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-10 22:38:09 +0200

    Throw exception instead of break (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7515).

--- coders/png.c
+++ coders/png.c
@@ -5277,7 +5277,11 @@ static Image *ReadOneMNGImage(MngInfo* mng_info, const ImageInfo *image_info,
 
               c=ReadBlobByte(image);
               if (c == EOF)
-                break;
+                {
+                  chunk=(unsigned char *) RelinquishMagickMemory(chunk);
+                  ThrowReaderException(CorruptImageError,
+                    "InsufficientImageDataInFile");
+                }
               chunk[i]=(unsigned char) c;
             }
 
