84d5a017243f1bf6176352f30af9980c97c97b6f
Cristy <urban-warrior@imagemagick.org>
2018-05-19 14:39:37 -0400

    ...

--- magick/draw.c
+++ magick/draw.c
@@ -3138,7 +3138,7 @@ MagickExport MagickBooleanType DrawImage(Image *image,const DrawInfo *draw_info)
                     continue;
                   break;
                 }
-                if ((size_t) (q-p-4+1) <= 0)
+                if ((q == (char *) NULL) || (p == (char *) NULL) || ((q-4) < p))
                   {
                     status=MagickFalse;
                     break;
