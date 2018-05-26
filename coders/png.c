5c33f74ca644b5f187f93509d2cf51018e995bbc
Dirk Lemstra <dirk@git.imagemagick.org>
2018-05-26 11:22:42 +0200

    Fixed determining PNG color_type when image type is set to grayscale.

--- coders/png.c
+++ coders/png.c
@@ -9992,27 +9992,20 @@ static MagickBooleanType WriteOnePNGImage(MngInfo *mng_info,
              (void) LogMagickEvent(CoderEvent,GetMagickModule(),
              "  Selecting PNG colortype:");
 
-          ping_color_type=(png_byte) ((matte != MagickFalse)?
-            PNG_COLOR_TYPE_RGB_ALPHA:PNG_COLOR_TYPE_RGB);
-
           if (image_info->type == TrueColorType)
             {
               ping_color_type=(png_byte) PNG_COLOR_TYPE_RGB;
               image_matte=MagickFalse;
             }
-
-          if (image_info->type == TrueColorMatteType)
+          else if (image_info->type == TrueColorMatteType)
             {
               ping_color_type=(png_byte) PNG_COLOR_TYPE_RGB_ALPHA;
               image_matte=MagickTrue;
             }
-
-          if (image_info->type == PaletteType ||
-              image_info->type == PaletteMatteType)
+          else if (image_info->type == PaletteType ||
+                   image_info->type == PaletteMatteType)
             ping_color_type=(png_byte) PNG_COLOR_TYPE_PALETTE;
-
-          if (mng_info->write_png_colortype == 0 &&
-             image_info->type == UndefinedType)
+          else
             {
               if (ping_have_color == MagickFalse)
                 {
