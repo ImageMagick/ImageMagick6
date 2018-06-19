d47e0e07da9daf0c923040280ba7e5bad1b44aef
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-19 15:02:32 -0700

    `THREAD_SUPPORT` and `WINDOWS_SUPPORT` are not mutually exclusive
    When cross-compiling libImageMagick for windows from a Linux system
    using mingw32 compilers, it is possible to have `pthread` support while
    still targeting the windows platform.

--- magick/studio.h
+++ magick/studio.h
@@ -125,7 +125,8 @@ extern "C" {
 #endif
 #if defined(MAGICKCORE_THREAD_SUPPORT)
 # include <pthread.h>
-#elif defined(MAGICKCORE_WINDOWS_SUPPORT)
+#endif
+#if defined(MAGICKCORE_WINDOWS_SUPPORT)
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
