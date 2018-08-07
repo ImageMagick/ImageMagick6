4f1f69d3d93d76882930ac71a1f200b2c29bd47c
Cristy <urban-warrior@imagemagick.org>
2018-08-06 20:26:52 -0400

    ...

--- coders/heic.c
+++ coders/heic.c
@@ -618,10 +618,7 @@ static MagickBooleanType WriteHEICImage(const ImageInfo *image_info,Image *image
     if (status == MagickFalse)
       break;
     if (GetNextImageInList(image) == (Image *) NULL)
-      {
-        status=MagickFalse;
-        break;
-      }
+      break;
     image=SyncNextImageInList(image);
     status=SetImageProgress(image,SaveImagesTag,scene,
       GetImageListLength(image));
