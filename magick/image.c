43952aa054a5cee990b3f46e80b93d6024abece9
Cristy <urban-warrior@imagemagick.org>
2018-08-08 07:12:06 -0400

    ...

--- magick/image.c
+++ magick/image.c
@@ -2795,9 +2795,6 @@ MagickExport MagickBooleanType SetImageInfo(ImageInfo *image_info,
         }
       else
         {
-          register size_t
-            n;
-
           size_t
             first,
             last;
@@ -2806,8 +2803,7 @@ MagickExport MagickBooleanType SetImageInfo(ImageInfo *image_info,
           image_info->scene=StringToUnsignedLong(image_info->scenes);
           image_info->number_scenes=image_info->scene;
           p=image_info->scenes;
-          q=(char *) image_info->scenes;
-          for (n=0; *q != '\0'; p++, n++)
+          for (q=(char *) image_info->scenes; *q != '\0'; p++)
           {
             while ((isspace((int) ((unsigned char) *p)) != 0) || (*p == ','))
               p++;
@@ -2826,8 +2822,6 @@ MagickExport MagickBooleanType SetImageInfo(ImageInfo *image_info,
             p=q;
           }
           image_info->number_scenes-=image_info->scene-1;
-          if ((image_info->number_scenes == 1) && (n > 1))
-            image_info->number_scenes++;
           image_info->subimage=image_info->scene;
           image_info->subrange=image_info->number_scenes;
         }
