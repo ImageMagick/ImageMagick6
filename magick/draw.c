ab5060a80f95bf044f64fba1aa06000896d9970e
Cristy <urban-warrior@imagemagick.org>
2018-09-16 10:04:14 -0400

    Further SVG compliance

--- magick/draw.c
+++ magick/draw.c
@@ -3638,6 +3638,7 @@ static MagickBooleanType RenderMVGContent(Image *image,
         if (LocaleCompare("text",keyword) == 0)
           {
             primitive_type=TextPrimitive;
+            cursor=0.0;
             break;
           }
         if (LocaleCompare("text-align",keyword) == 0)
