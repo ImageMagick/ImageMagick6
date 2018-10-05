394b3e6edf74d1337ce338927da053bb40c00ae9
Cristy <urban-warrior@imagemagick.org>
2018-10-05 19:26:41 -0400

    ...

--- coders/pict.c
+++ coders/pict.c
@@ -1790,7 +1790,7 @@ static MagickBooleanType WritePICTImage(const ImageInfo *image_info,
   /*
     Allocate memory.
   */
-  bytes_per_line=image->columns | 0x8000;
+  bytes_per_line=image->columns;
   if (storage_class == DirectClass)
     bytes_per_line*=image->matte != MagickFalse ? 4 : 3;
   buffer=(unsigned char *) AcquireQuantumMemory(PictInfoSize,sizeof(*buffer));
@@ -1811,7 +1811,8 @@ static MagickBooleanType WritePICTImage(const ImageInfo *image_info,
       ThrowWriterException(ResourceLimitError,"MemoryAllocationFailed");
     }
   (void) memset(scanline,0,row_bytes);
-  (void) memset(packed_scanline,0,(size_t) (row_bytes+2*MaxCount));
+  (void) memset(packed_scanline,0,(size_t) (row_bytes+2*MaxCount)*
+    sizeof(*packed_scanline));
   /*
     Write header, header size, size bounding box, version, and reserved.
   */
