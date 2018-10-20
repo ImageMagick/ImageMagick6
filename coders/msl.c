bb77f9e905597c7ab1e92042c7de418d999b00bf
Dirk Lemstra <dirk@lemstra.org>
2018-10-20 09:23:33 +0200

    Fixed leaking of the image when writing an MSL image.

--- coders/msl.c
+++ coders/msl.c
@@ -8287,6 +8287,9 @@ static MagickBooleanType WriteMSLImage(const ImageInfo *image_info,Image *image)
   Image
     *msl_image;
 
+  MagickBooleanType
+    status;
+
   assert(image_info != (const ImageInfo *) NULL);
   assert(image_info->signature == MagickCoreSignature);
   assert(image != (Image *) NULL);
@@ -8294,6 +8297,8 @@ static MagickBooleanType WriteMSLImage(const ImageInfo *image_info,Image *image)
   if (image->debug != MagickFalse)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
   msl_image=CloneImage(image,0,0,MagickTrue,&image->exception);
-  return(ProcessMSLScript(image_info,&msl_image,&image->exception));
+  status=ProcessMSLScript(image_info,&msl_image,&image->exception);
+  msl_image=DestroyImageList(msl_image);
+  return(status);
 }
 #endif
