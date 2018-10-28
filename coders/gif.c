9b6c83c9a5157de89a54d4725f0c2ff275b813a5
Cristy <urban-warrior@imagemagick.org>
2018-10-27 20:29:27 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11155

--- coders/gif.c
+++ coders/gif.c
@@ -1123,7 +1123,7 @@ static Image *ReadGIFImage(const ImageInfo *image_info,ExceptionInfo *exception)
                     ThrowGIFException(ResourceLimitError,
                       "MemoryAllocationFailed");
                 }
-              (void) CopyMagickMemory(&comments[offset],(char *) buffer,extent-
+              (void) CopyMagickString(&comments[offset],(char *) buffer,extent-
                 offset);
             }
             (void) SetImageProperty(meta_image,"comment",comments);
