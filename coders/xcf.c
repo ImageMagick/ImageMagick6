ddd5bd95d9ab45280b275879e66558596571a1aa
Cristy <urban-warrior@imagemagick.org>
2018-12-01 12:24:02 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11641

--- coders/xcf.c
+++ coders/xcf.c
@@ -1114,6 +1114,8 @@ static Image *ReadXCFImage(const ImageInfo *image_info,ExceptionInfo *exception)
       InheritException(exception,&image->exception);
       return(DestroyImageList(image));
     }
+  if (status != MagickFalse)
+    status=ResetImagePixels(image,exception);
   if (image_type == GIMP_INDEXED)
     ThrowReaderException(CoderError,"ColormapTypeNotSupported");
   if (image_type == GIMP_RGB)
