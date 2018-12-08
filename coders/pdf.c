05beca2acd0b6cb6991887feb8ebf52202b86680
Cristy <urban-warrior@imagemagick.org>
2018-12-08 15:45:08 -0500

    https://github.com/ImageMagick/ImageMagick/issues/1409

--- coders/pdf.c
+++ coders/pdf.c
@@ -1370,9 +1370,6 @@ RestoreMSCWarning
   unsigned char
     *pixels;
 
-  wchar_t
-    *utf16;
-
   /*
     Open output image file.
   */
@@ -2951,17 +2948,26 @@ RestoreMSCWarning
     object);
   (void) WriteBlobString(image,buffer);
   (void) WriteBlobString(image,"<<\n");
-  utf16=ConvertUTF8ToUTF16((unsigned char *) basename,&length);
-  if (utf16 != (wchar_t *) NULL)
+  if (LocaleCompare(image_info->magick,"PDFA") == 0)
+    (void) FormatLocaleString(buffer,MagickPathExtent,"/Title (%s)\n",
+      EscapeParenthesis(basename));
+  else
     {
-      (void) FormatLocaleString(buffer,MaxTextExtent,"/Title (\xfe\xff");
-      (void) WriteBlobString(image,buffer);
-      for (i=0; i < (ssize_t) length; i++)
-        (void) WriteBlobMSBShort(image,(unsigned short) utf16[i]);
-      (void) FormatLocaleString(buffer,MaxTextExtent,")\n");
-      (void) WriteBlobString(image,buffer);
-      utf16=(wchar_t *) RelinquishMagickMemory(utf16);
+      wchar_t
+        *utf16;
+
+      utf16=ConvertUTF8ToUTF16((unsigned char *) basename,&length);
+      if (utf16 != (wchar_t *) NULL)
+        {
+          (void) FormatLocaleString(buffer,MagickPathExtent,"/Title (\xfe\xff");
+          (void) WriteBlobString(image,buffer);
+          for (i=0; i < (ssize_t) length; i++)
+            (void) WriteBlobMSBShort(image,(unsigned short) utf16[i]);
+          (void) FormatLocaleString(buffer,MagickPathExtent,")\n");
+          utf16=(wchar_t *) RelinquishMagickMemory(utf16);
+        }
     }
+  (void) WriteBlobString(image,buffer);
   seconds=time((time_t *) NULL);
 #if defined(MAGICKCORE_HAVE_LOCALTIME_R)
   (void) localtime_r(&seconds,&local_time);
