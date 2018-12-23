22bbd7fe9326ea8d3e56870c2e3d25ace8cf19bd
Cristy <urban-warrior@imagemagick.org>
2018-12-23 18:34:38 -0500

    ...

--- magick/cache.c
+++ magick/cache.c
@@ -1012,9 +1012,8 @@ static void DestroyImagePixelCache(Image *image)
   assert(image->signature == MagickCoreSignature);
   if (image->debug != MagickFalse)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
-  if (image->cache == (void *) NULL)
-    return;
-  image->cache=DestroyPixelCache(image->cache);
+  if (image->cache != (void *) NULL)
+    image->cache=DestroyPixelCache(image->cache);
 }
 
 /*
