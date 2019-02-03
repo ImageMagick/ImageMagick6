39b036025b9db0789eac02bbdf20415f82156db8
Cristy <mikayla-grace@urban-warrior.org>
2019-02-02 19:59:01 -0500

    https://github.com/ImageMagick/ImageMagick/issues/1469

--- coders/png.c
+++ coders/png.c
@@ -3781,7 +3781,14 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
             if (!png_get_valid(ping,ping_info,PNG_INFO_pHYs) ||
                 (LocaleCompare(text[i].key,"density") != 0 &&
                 LocaleCompare(text[i].key,"units") != 0))
-               (void) SetImageProperty(image,text[i].key,value);
+              {
+                char
+                  key[MaxTextExtent];
+
+                (void) FormatLocaleString(key,MaxTextExtent,"png:%s",
+                  text[i].key);
+                (void) SetImageProperty(image,key,value);
+              }
 
             if (logging != MagickFalse)
             {
