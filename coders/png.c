445492ffff82fc46d1d2dc8dc8d837ec1957f3dc
Cristy <urban-warrior@imagemagick.org>
2019-04-03 07:51:03 -0400

    ...

--- coders/png.c
+++ coders/png.c
@@ -3621,7 +3621,8 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
     {
       png_destroy_read_struct(&ping,&ping_info,&end_info);
       pixel_info=RelinquishVirtualMemory(pixel_info);
-      image->colors=2;
+      if (AcquireImageColormap(image,2) == MagickFalse)
+        ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
       (void) SetImageBackgroundColor(image);
 #ifdef IMPNG_SETJMP_NOT_THREAD_SAFE
       UnlockSemaphoreInfo(ping_semaphore);
