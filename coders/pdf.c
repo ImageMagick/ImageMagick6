951a7553b5ed51f97b09b13de2b85e9d8fa30f60
Cristy <mikayla-grace@urban-warrior.org>
2019-04-17 17:49:07 -0400

    Decorate GetLocalTime() to avoid namespace collision

--- coders/pdf.c
+++ coders/pdf.c
@@ -3000,7 +3000,7 @@ RestoreMSCWarning
     }
   (void) WriteBlobString(image,buffer);
   seconds=GetMagickTime();
-  GetUTCTime(&seconds,&local_time);
+  GetMagickUTCtime(&seconds,&local_time);
   (void) FormatLocaleString(date,MaxTextExtent,"D:%04d%02d%02d%02d%02d%02d",
     local_time.tm_year+1900,local_time.tm_mon+1,local_time.tm_mday,
     local_time.tm_hour,local_time.tm_min,local_time.tm_sec);
