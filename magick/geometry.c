5bb4d8b50287c6f1dcede2b3d09b5ee7da299780
Cristy <mikayla-grace@urban-warrior.org>
2019-04-21 11:03:47 -0400

    Revert geometry patch

--- magick/geometry.c
+++ magick/geometry.c
@@ -543,7 +543,7 @@ MagickExport void GravityAdjustGeometry(const size_t width,
     case EastGravity:
     case SouthEastGravity:
     {
-      region->x+=(ssize_t) (width-region->width);
+      region->x=(ssize_t) (width-region->width-region->x);
       break;
     }
     case NorthGravity:
@@ -567,7 +567,7 @@ MagickExport void GravityAdjustGeometry(const size_t width,
     case SouthGravity:
     case SouthEastGravity:
     {
-      region->y+=(ssize_t) (height-region->height);
+      region->y=(ssize_t) (height-region->height-region->y);
       break;
     }
     case EastGravity:
