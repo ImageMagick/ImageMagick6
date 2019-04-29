62909e8fd391d02713f3742fe368a4be03342c44
Cristy <mikayla-grace@urban-warrior.org>
2019-04-29 10:04:39 -0400

    https://imagemagick.org/discourse-server/viewtopic.php?f=1&t=35939

--- magick/fx.c
+++ magick/fx.c
@@ -2392,8 +2392,9 @@ static double FxEvaluateSubexpression(FxInfo *fx_info,const ChannelType channel,
         }
         case ',':
         {
-          *beta=FxEvaluateSubexpression(fx_info,channel,x,y,++p,depth+1,beta,
-            exception);
+          if (fabs(alpha) > MagickEpsilon)
+            *beta=FxEvaluateSubexpression(fx_info,channel,x,y,++p,depth+1,beta,
+              exception);
           FxReturn(alpha);
         }
         case ';':
