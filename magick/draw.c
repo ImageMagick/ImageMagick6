61051c45ccea9f0623b72a493c9259c7cc3ee819
Cristy <mikayla-grace@urban-warrior.org>
2019-05-24 19:13:13 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1578

--- magick/draw.c
+++ magick/draw.c
@@ -3674,7 +3674,6 @@ static MagickBooleanType RenderMVGContent(Image *image,
         if (LocaleCompare("text",keyword) == 0)
           {
             primitive_type=TextPrimitive;
-            cursor=0.0;
             break;
           }
         if (LocaleCompare("text-align",keyword) == 0)
