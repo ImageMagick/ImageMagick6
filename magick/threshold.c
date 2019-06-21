55e6dc49f1a381d9d511ee2f888fdc3e3c3e3953
Cristy <urban-warrior@imagemagick.org>
2019-06-21 16:52:14 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1608

--- magick/threshold.c
+++ magick/threshold.c
@@ -202,6 +202,8 @@ MagickExport Image *AdaptiveThresholdImage(const Image *image,
   threshold_image=CloneImage(image,0,0,MagickTrue,exception);
   if (threshold_image == (Image *) NULL)
     return((Image *) NULL);
+  if (width == 0)
+    return(threshold_image);
   if (SetImageStorageClass(threshold_image,DirectClass) == MagickFalse)
     {
       InheritException(exception,&threshold_image->exception);
