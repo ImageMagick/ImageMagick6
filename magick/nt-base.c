0ffeae194c0153a1a4e4d45b92fba819e6239b6d
Cristy <urban-warrior@imagemagick.org>
2019-07-14 09:34:20 -0400

    Eliminate compiler warning

--- magick/nt-base.c
+++ magick/nt-base.c
@@ -1693,8 +1693,8 @@ MagickExport MagickBooleanType NTLongPathsEnabled()
           RegCloseKey(registry_key);
           return(MagickFalse);
         }
-      status=RegQueryValueExA(registry_key,"LongPathsEnabled",0,&type,&value,
-        &size);
+      status=RegQueryValueExA(registry_key,"LongPathsEnabled",0,&type,
+        (LPBYTE) &value,&size);
       RegCloseKey(registry_key);
       if (status != ERROR_SUCCESS)
         {
