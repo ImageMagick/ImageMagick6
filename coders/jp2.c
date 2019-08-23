9db97d2bb02231dec8f6be96a10e9f1e25f37880
Dirk Lemstra <dirk@lemstra.org>
2019-08-23 10:59:53 +0200

    Corrected numresolution calculation ImageMagick/ImageMagick#1673.

--- coders/jp2.c
+++ coders/jp2.c
@@ -794,8 +794,7 @@ static inline int CalculateNumResolutions(size_t width,size_t height)
     i;
 
   for (i=1; i < 6; i++)
-    if ((((size_t) 1UL << (i+2)) > width) &&
-        (((size_t) 1UL << (i+2)) > height))
+    if ((width < ((size_t) 1UL << i)) || (height < ((size_t) 1UL << i)))
       break;
   return(i);
 }
