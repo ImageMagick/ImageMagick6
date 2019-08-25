8d728a6efae47ed6a80f09e397bb2550a9f85715
Cristy <urban-warrior@imagemagick.org>
2019-08-24 21:14:37 -0400

    ...

--- magick/property.c
+++ magick/property.c
@@ -1631,6 +1631,14 @@ static MagickBooleanType GetEXIFProperty(const Image *image,
                 directory_stack[level].entry=entry;
                 directory_stack[level].offset=tag_offset;
                 level++;
+                /*
+                  Check for duplicate tag.
+                */
+                for (i=0; i < level; i++)
+                  if (directory_stack[i].directory == (exif+tag_offset1))
+                    break;
+                if (i < level)
+                  break;  /* duplicate tag */
                 directory_stack[level].directory=exif+offset;
                 directory_stack[level].offset=tag_offset1;
                 directory_stack[level].entry=0;
