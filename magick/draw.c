92d7e8dc1ee92a5ad8026691457db70876bc17e9
Cristy <urban-warrior@imagemagick.org>
2019-10-12 08:34:22 -0400

    ...

--- magick/draw.c
+++ magick/draw.c
@@ -4963,10 +4963,10 @@ RestoreMSCWarning
 
 static inline double ConstrainCoordinate(double x)
 {
-  if (x < (double) -SSIZE_MAX)
-    return((double) -SSIZE_MAX);
-  if (x > (double) SSIZE_MAX)
-    return((double) SSIZE_MAX);
+  if (x < (double) -(SSIZE_MAX-512))
+    return((double) -(SSIZE_MAX-512));
+  if (x > (double) (SSIZE_MAX-512))
+    return((double) (SSIZE_MAX-512));
   return(x);
 }
 
