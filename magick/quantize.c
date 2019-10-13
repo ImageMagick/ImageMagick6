2000dd1a7da8098483b8937b53ff3b6ff3048c97
Cristy <urban-warrior@imagemagick.org>
2019-10-13 12:16:40 -0400

    ...

--- magick/quantize.c
+++ magick/quantize.c
@@ -3218,8 +3218,8 @@ static int IntensityCompare(const void *x,const void *y)
 
   color_1=(PixelPacket *) x;
   color_2=(PixelPacket *) y;
-  intensity=(ssize_t) (PixelPacketIntensity(color_1)-
-    PixelPacketIntensity(color_2));
+  intensity=(ssize_t) PixelPacketIntensity(color_1)-
+    (ssize_t) PixelPacketIntensity(color_2));
   return((int) intensity);
 }
 
