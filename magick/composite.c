203682abb7e7e54ea216aba8592419db9baf283d
Cristy <urban-warrior@imagemagick.org>
2019-11-22 19:33:48 -0500

    Composition normally operates in the sRGB colorspace, but not for Copy*

--- magick/composite.c
+++ magick/composite.c
@@ -1648,9 +1648,24 @@ MagickExport MagickBooleanType CompositeImageChannel(Image *image,
   source_image=CloneImage(composite,0,0,MagickTrue,exception);
   if (source_image == (const Image *) NULL)
     return(MagickFalse);
-  if ((compose != CopyCompositeOp) &&
-      (IsGrayColorspace(image->colorspace) == MagickFalse))
-    (void) SetImageColorspace(image,sRGBColorspace);
+  switch (compose)
+  {
+    case CopyCompositeOp:
+    case CopyRedCompositeOp:
+    case CopyGreenCompositeOp:
+    case CopyBlueCompositeOp:
+    case CopyCyanCompositeOp:
+    case CopyMagentaCompositeOp:
+    case CopyYellowCompositeOp:
+    case CopyBlackCompositeOp:
+      break;
+    default:
+    {
+      if (IsGrayColorspace(image->colorspace) == MagickFalse)
+        (void) SetImageColorspace(image,sRGBColorspace);
+      break;
+    }
+  }
   (void) SetImageColorspace(source_image,image->colorspace);
   GetMagickPixelPacket(image,&zero);
   canvas_image=(Image *) NULL;
