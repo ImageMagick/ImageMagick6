aa299cf73d69cb4787fafb9de255469319c9a931
Cristy <mikayla-grace@urban-warrior.org>
2020-01-19 20:53:35 -0500

    ...

--- magick/draw.c
+++ magick/draw.c
@@ -997,9 +997,10 @@ static PolygonInfo *DestroyPolygonInfo(PolygonInfo *polygon_info)
   register ssize_t
     i;
 
-  for (i=0; i < (ssize_t) polygon_info->number_edges; i++)
-    polygon_info->edges[i].points=(PointInfo *)
-      RelinquishMagickMemory(polygon_info->edges[i].points);
+  if (polygon_info->edges != (EdgeInfo *) NULL)
+    for (i=0; i < (ssize_t) polygon_info->number_edges; i++)
+      polygon_info->edges[i].points=(PointInfo *)
+        RelinquishMagickMemory(polygon_info->edges[i].points);
   polygon_info->edges=(EdgeInfo *) RelinquishMagickMemory(polygon_info->edges);
   return((PolygonInfo *) RelinquishMagickMemory(polygon_info));
 }
