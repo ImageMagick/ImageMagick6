9e456df6dde6898d76cf9de179976ddd35e9a090
Cristy <mikayla-grace@urban-warrior.org>
2020-01-24 19:40:37 -0500

    ...

--- coders/tiff.c
+++ coders/tiff.c
@@ -1677,18 +1677,14 @@ RestoreMSCWarning
       }
     if (rows_per_strip > (uint32) image->rows)
       rows_per_strip=(uint32) image->rows;
-    if ((photometric != PHOTOMETRIC_RGB) &&
-        (photometric != PHOTOMETRIC_CIELAB) &&
-        (photometric != PHOTOMETRIC_SEPARATED))
-      method=ReadGenericMethod;
-    if ((photometric != PHOTOMETRIC_SEPARATED) &&
-        (interlace == PLANARCONFIG_SEPARATE) && (bits_per_sample < 64))
-      method=ReadGenericMethod;
+    method=ReadGenericMethod;
     if (TIFFIsTiled(tiff) != MagickFalse)
       method=ReadTileMethod;
     else
       if (TIFFGetField(tiff,TIFFTAG_ROWSPERSTRIP,&rows_per_strip) == 1)
         method=ReadStripMethod;
+    if (photometric == PHOTOMETRIC_LOGLUV)
+      method=ReadGenericMethod;
     if (image->compression == JPEGCompression)
       method=GetJPEGMethod(image,tiff,photometric,bits_per_sample,
         samples_per_pixel);
