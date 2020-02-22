4372b8636928156ecc6000524990fac8491b1f4f
Dirk Lemstra <dirk@lemstra.org>
2020-02-22 11:17:26 +0100

    Removed unused NTSeekDirectory.

--- magick/nt-base-private.h
+++ magick/nt-base-private.h
@@ -135,7 +135,6 @@ extern MagickPrivate void
   NTInitializeWinsock(MagickBooleanType),
   *NTMapMemory(char *,size_t,int,int,int,MagickOffsetType),
   *NTOpenLibrary(const char *),
-  NTSeekDirectory(DIR *,ssize_t),
   NTWindowsGenesis(void),
   NTWindowsTerminus(void);
 
