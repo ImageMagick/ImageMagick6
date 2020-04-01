ce6ef690743fd1880067b170a2efdd78e01323e3
Dirk Lemstra <dirk@lemstra.org>
2020-04-01 20:16:25 +0200

    Improved readability of it statement.

--- magick/annotate.c
+++ magick/annotate.c
@@ -1754,14 +1754,15 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
         (IsUTFSpace(GetUTFCode(p+grapheme[i].cluster)) != MagickFalse) &&
         (IsUTFSpace(code) == MagickFalse))
       origin.x+=(FT_Pos) (64.0*draw_info->interword_spacing);
-    else
-      if (i != last_character)
-        origin.x+=(FT_Pos) grapheme[i].x_advance;
-      else
+    else if (i == last_character)
+      {
         if (bounds.xMax == 0)
           origin.x+=(FT_Pos) grapheme[i].x_advance;
         else
           origin.x+=(FT_Pos) bounds.xMax;
+      }
+    else
+      origin.x+=(FT_Pos) grapheme[i].x_advance;
     metrics->origin.x=(double) origin.x;
     metrics->origin.y=(double) origin.y;
     if (metrics->origin.x > metrics->width)
