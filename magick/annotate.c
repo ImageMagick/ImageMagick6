1860dd969402a0dbf265cf51a8fc7f40f53862f7
Cristy <mikayla-grace@urban-warrior.org>
2020-04-25 21:20:27 -0400

    Label was not centered properly

--- magick/annotate.c
+++ magick/annotate.c
@@ -1754,15 +1754,16 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
         (IsUTFSpace(GetUTFCode(p+grapheme[i].cluster)) != MagickFalse) &&
         (IsUTFSpace(code) == MagickFalse))
       origin.x+=(FT_Pos) (64.0*draw_info->interword_spacing);
-    else if (i == last_character)
-      {
-        if (bounds.xMax == 0)
-          origin.x+=(FT_Pos) grapheme[i].x_advance;
-        else
-          origin.x+=(FT_Pos) bounds.xMax;
-      }
     else
-      origin.x+=(FT_Pos) grapheme[i].x_advance;
+      if (i == last_character)
+        {
+          if ((bounds.xMax == 0) || (draw_info->render == MagickFalse))
+            origin.x+=(FT_Pos) grapheme[i].x_advance;
+          else
+            origin.x+=(FT_Pos) bounds.xMax;
+        }
+      else
+        origin.x+=(FT_Pos) grapheme[i].x_advance;
     metrics->origin.x=(double) origin.x;
     metrics->origin.y=(double) origin.y;
     if (metrics->origin.x > metrics->width)
