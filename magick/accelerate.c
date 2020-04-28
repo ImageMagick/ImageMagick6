3fb00862ce18133f6ff0b45a343d55b32e9c08b5
Dirk Lemstra <dirk@lemstra.org>
2020-04-28 21:06:19 +0200

    Disabled OpenCL AccelerateAddNoiseImage because of the repetition in the work groups.

--- magick/accelerate.c
+++ magick/accelerate.c
@@ -448,6 +448,8 @@ MagickPrivate Image *AccelerateAddNoiseImage(const Image *image,
   const ChannelType channel,const NoiseType noise_type,
   ExceptionInfo *exception) 
 {
+  /* Temporary disabled because of repetition.
+
   Image
     *filteredImage;
 
@@ -461,6 +463,12 @@ MagickPrivate Image *AccelerateAddNoiseImage(const Image *image,
   filteredImage = ComputeAddNoiseImage(image,channel,noise_type,exception);
   
   return(filteredImage);
+  */
+  magick_unreferenced(image);
+  magick_unreferenced(channel);
+  magick_unreferenced(noise_type);
+  magick_unreferenced(exception);
+  return((Image *)NULL);
 }
 
 /*
