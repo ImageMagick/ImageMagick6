ffd51807420d60280e35f64c14744e132bc4d74a
Dirk Lemstra <dirk@lemstra.org>
2020-07-17 16:45:43 +0200

    Improved reading of PSD layers and added extra logic to determine if the merged image has an alpha channel.

--- coders/psd.c
+++ coders/psd.c
@@ -1595,6 +1595,44 @@ static void CheckMergedImageAlpha(const PSDInfo *psd_info,Image *image)
     image->matte=MagickTrue;
 }
 
+static MagickSizeType GetLayerInfoSize(const PSDInfo *psd_info,Image *image)
+{
+  char
+    type[4];
+
+  MagickSizeType
+    size;
+
+  ssize_t
+    count;
+
+  size=GetPSDSize(psd_info,image);
+  if (size != 0)
+    return(size);
+  (void) ReadBlobLong(image);
+  count=ReadPSDString(image,type,4);
+  if ((count != 4) || (LocaleNCompare(type,"8BIM",4) != 0))
+    return(0);
+  count=ReadPSDString(image,type,4);
+  if ((count == 4) && ((LocaleNCompare(type,"Mt16",4) == 0) ||
+      (LocaleNCompare(type,"Mt32",4) == 0) ||
+      (LocaleNCompare(type,"Mtrn",4) == 0)))
+    {
+      size=GetPSDSize(psd_info,image);
+      if (size != 0)
+        return(0);
+      image->matte=MagickTrue;
+      count=ReadPSDString(image,type,4);
+      if ((count != 4) || (LocaleNCompare(type,"8BIM",4) != 0))
+        return(0);
+      count=ReadPSDString(image,type,4);
+    }
+  if ((count == 4) && ((LocaleNCompare(type,"Lr16",4) == 0) ||
+      (LocaleNCompare(type,"Lr32",4) == 0)))
+    size=GetPSDSize(psd_info,image);
+  return(size);
+}
+
 static MagickBooleanType ReadPSDLayersInternal(Image *image,
   const ImageInfo *image_info,const PSDInfo *psd_info,
   const MagickBooleanType skip_layers,ExceptionInfo *exception)
@@ -1619,32 +1657,7 @@ static MagickBooleanType ReadPSDLayersInternal(Image *image,
     j,
     number_layers;
 
-  size=GetPSDSize(psd_info,image);
-  if (size == 0)
-    {
-      /*
-        Skip layers & masks.
-      */
-      (void) ReadBlobLong(image);
-      count=ReadPSDString(image,type,4);
-      if ((count != 4) || (LocaleNCompare(type,"8BIM",4) != 0))
-        {
-          CheckMergedImageAlpha(psd_info,image);
-          return(MagickTrue);
-        }
-      else
-        {
-          count=ReadPSDString(image,type,4);
-          if ((count == 4) && ((LocaleNCompare(type,"Lr16",4) == 0) ||
-              (LocaleNCompare(type,"Lr32",4) == 0)))
-            size=GetPSDSize(psd_info,image);
-          else
-            {
-               CheckMergedImageAlpha(psd_info,image);
-              return(MagickTrue);
-            }
-        }
-    }
+  size=GetLayerInfoSize(psd_info,image);
   status=MagickTrue;
   if (size != 0)
     {
