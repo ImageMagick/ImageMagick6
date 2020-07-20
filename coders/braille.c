162eb0fbbec9f856d2362ceff643564df2f3f44f
Cristy <mikayla-grace@urban-warrior.org>
2020-07-20 07:34:45 -0400

    eliminate compiler warning

--- coders/braille.c
+++ coders/braille.c
@@ -185,6 +185,14 @@ ModuleExport void UnregisterBRAILLEImage(void)
 static MagickBooleanType WriteBRAILLEImage(const ImageInfo *image_info,
   Image *image)
 {
+#define do_cell(dx,dy,bit) \
+{ \
+  if (image->storage_class == PseudoClass) \
+    cell|=(GetPixelIndex(indexes+x+dx+dy*image->columns) == polarity) << bit; \
+  else \
+    cell|=(GetPixelGreen(p+x+dx+dy*image->columns) == 0) << bit; \
+}
+
   char
     buffer[MaxTextExtent];
 
@@ -302,12 +310,6 @@ static MagickBooleanType WriteBRAILLEImage(const ImageInfo *image_info,
         cell = 0;
 
       two_columns=(x+1 < (ssize_t) image->columns) ? MagickTrue : MagickFalse;
-#define do_cell(dx,dy,bit) \
-      if (image->storage_class == PseudoClass) \
-        cell |= (GetPixelIndex(indexes+x+dx+dy*image->columns) == polarity) << bit; \
-      else \
-        cell |= (GetPixelGreen(p+x+dx+dy*image->columns) == 0) << bit;
-
       do_cell(0,0,0)
       if (two_columns != MagickFalse)
         do_cell(1,0,3)
