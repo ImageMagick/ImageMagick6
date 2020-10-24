e20a06d7205e9229bd99144b0c1302348e02a2dd
Cristy <urban-warrior@imagemagick.org>
2020-10-24 20:05:11 +0000

    ...

--- magick/property.c
+++ magick/property.c
@@ -3265,10 +3265,17 @@ MagickExport const char *GetMagickProperty(const ImageInfo *image_info,
           const char
             *papersize;
 
-          papersize=GetPageGeometry(property+10);
           *value='\0';
+          papersize=GetPageGeometry(property+10);
           if (papersize != (const char *) NULL)
-            (void) CopyMagickString(value,papersize,MagickPathExtent);
+            {
+              RectangleInfo
+                page;
+
+              (void) ParseAbsoluteGeometry(papersize,&page);
+              (void) FormatLocaleString(value,MaxTextExtent,"%.20gx%.20g",
+                (double) page.width,(double) page.height);
+            }
           break;
         }
 #if defined(MAGICKCORE_LCMS_DELEGATE)
