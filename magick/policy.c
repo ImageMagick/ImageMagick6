79a97bd210314e5c3cf8c90123c9337ae299d968
Cristy <urban-warrior@imagemagick.org>
2020-11-22 22:47:21 +0000

    ...

--- magick/policy.c
+++ magick/policy.c
@@ -454,8 +454,8 @@ static char *AcquirePolicyString(const char *source)
   return(destination);
 }
 
-MagickExport char **GetPolicyList(const char *pattern,
-  size_t *number_policies,ExceptionInfo *exception)
+MagickExport char **GetPolicyList(const char *pattern,size_t *number_policies,
+  ExceptionInfo *exception)
 {
   char
     **policies;
