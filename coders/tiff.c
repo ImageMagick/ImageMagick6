7d116a3bb2401561c85460d298684556999d9c37
Cristy <urban-warrior@imagemagick.org>
2020-12-18 13:09:35 +0000

    https://github.com/ImageMagick/ImageMagick/discussions/2935

--- coders/tiff.c
+++ coders/tiff.c
@@ -1748,7 +1748,7 @@ static Image *ReadTIFFImage(const ImageInfo *image_info,
       ThrowTIFFException(ResourceLimitError,"MemoryAllocationFailed");
     pixels=(unsigned char *) GetVirtualMemoryBlob(pixel_info);
     (void) ResetMagickMemory(pixels,0,number_pixels*sizeof(uint32));
-    quantum_type=GrayQuantum;
+    quantum_type=IndexQuantum;
     if (interlace != PLANARCONFIG_SEPARATE)
       {
         size_t
