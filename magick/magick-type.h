8b4078682b9fd4988ea44d077cf77eecb8f836d8
Cristy <mikayla-grace@urban-warrior.org>
2021-01-30 09:48:00 -0500

    bump SO per https://github.com/ImageMagick/ImageMagick/pull/2832

--- magick/magick-type.h
+++ magick/magick-type.h
@@ -39,9 +39,6 @@ extern "C" {
 #  define MagickULLConstant(c)  ((MagickSizeType) (c ## ULL))
 #endif
 
-#if defined(__s390__)
-typedef double MagickFloatType;
-#else
 #if MAGICKCORE_SIZEOF_FLOAT_T == 0
 typedef float MagickFloatType;
 #elif (MAGICKCORE_SIZEOF_FLOAT_T == MAGICKCORE_SIZEOF_FLOAT)
@@ -52,7 +49,6 @@ typedef double MagickFloatType;
 typedef double MagickFloatType;
 #else
 #error Your MagickFloatType type is neither a float, nor a double, nor a long double
-#endif
 #endif
 #if MAGICKCORE_SIZEOF_DOUBLE_T == 0
 typedef double MagickDoubleType;
