798838f31c0112c10491bf2cf2a4d084be90e26f
Cristy <mikayla-grace@urban-warrior.org>
2021-04-03 07:35:03 -0400

    prevent undefined-behavior issue reported by Hardik

--- magick/profile.c
+++ magick/profile.c
@@ -1967,18 +1967,18 @@ static MagickBooleanType Sync8BimProfile(Image *image,StringInfo *profile)
     if ((id == 0x3ED) && (count == 16))
       {
         if (image->units == PixelsPerCentimeterResolution)
-          WriteProfileLong(MSBEndian,(unsigned int) (image->x_resolution*2.54*
-            65536.0),p);
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->x_resolution*2.54*65536.0),p);
         else
-          WriteProfileLong(MSBEndian,(unsigned int) (image->x_resolution*
-            65536.0),p);
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->x_resolution*65536.0),p);
         WriteProfileShort(MSBEndian,(unsigned short) image->units,p+4);
         if (image->units == PixelsPerCentimeterResolution)
-          WriteProfileLong(MSBEndian,(unsigned int) (image->y_resolution*2.54*
-            65536.0),p+8);
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->y_resolution*2.54*65536.0),p+8);
         else
-          WriteProfileLong(MSBEndian,(unsigned int) (image->y_resolution*
-            65536.0),p+8);
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->y_resolution*65536.0),p+8);
         WriteProfileShort(MSBEndian,(unsigned short) image->units,p+12);
       }
     p+=count;
