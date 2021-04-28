5ee7e3eae5a0f4c06969c5b99a0a605901c0eac1
Cristy <mikayla-grace@urban-warrior.org>
2021-04-28 17:11:37 -0400

    check image is not NULL before dereferencing

--- coders/tiff.c
+++ coders/tiff.c
@@ -1659,7 +1659,7 @@ static Image *ReadTIFFImage(const ImageInfo *image_info,
     quantum_info->endian=LSBEndian;
     if (TIFFScanlineSize(tiff) <= 0)
       ThrowTIFFException(ResourceLimitError,"MemoryAllocationFailed");
-    if (((MagickSizeType) TIFFScanlineSize(tiff)) > (2.53*GetBlobSize(image)))
+    if (((MagickSizeType) TIFFScanlineSize(tiff)) > (2.55*GetBlobSize(image)))
       ThrowTIFFException(CorruptImageError,"InsufficientImageDataInFile");
     number_pixels=MagickMax(TIFFScanlineSize(tiff),MagickMax((ssize_t)
       image->columns*samples_per_pixel*pow(2.0,ceil(log(bits_per_sample)/
