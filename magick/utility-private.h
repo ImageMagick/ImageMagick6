8e9b75ea13bf007724d5e6dd5625f9af7d6098fa
Cristy <mikayla-grace@urban-warrior.org>
2021-05-12 08:16:30 -0400

    https://github.com/ImageMagick/ImageMagick/issues/3653

--- magick/utility-private.h
+++ magick/utility-private.h
@@ -32,14 +32,10 @@ extern MagickPrivate MagickBooleanType
 static inline int MagickReadDirectory(DIR *directory,struct dirent *entry,
   struct dirent **result)
 {
-#if defined(MAGICKCORE_HAVE_READDIR_R)
-  return(readdir_r(directory,entry,result));
-#else
   (void) entry;
   errno=0;
   *result=readdir(directory);
   return(errno);
-#endif
 }
 
 /*
