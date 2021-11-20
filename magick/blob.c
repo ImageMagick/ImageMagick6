7864d48ddb52c490484a890eea21717cab01c61c
Cristy <mikayla-grace@urban-warrior.org>
2021-11-19 21:47:03 -0500

    eliminate compiler warning

--- magick/blob.c
+++ magick/blob.c
@@ -2123,14 +2123,8 @@ MagickExport MagickBooleanType InjectImageBlob(const ImageInfo *image_info,
   MagickBooleanType
     status;
 
-  ssize_t
-    i;
-
   size_t
     quantum;
-
-  ssize_t
-    count;
 
   struct stat
     file_stats;
@@ -2206,9 +2200,9 @@ MagickExport MagickBooleanType InjectImageBlob(const ImageInfo *image_info,
       ThrowBinaryException(ResourceLimitError,"MemoryAllocationFailed",
         image->filename);
     }
-  for (i=0; ; i+=count)
+  for ( ; ; )
   {
-    count=read(file,buffer,quantum);
+    ssize_t count = read(file,buffer,quantum);
     if (count <= 0)
       {
         count=0;
