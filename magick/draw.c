ff04a51f4f89813c319f157c0aa993f36b944815
Cristy <urban-warrior@imagemagick.org>
2022-02-13 16:08:50 -0500

    alpha is never zero

--- magick/draw.c
+++ magick/draw.c
@@ -4732,7 +4732,7 @@ static double GetOpacityPixel(PolygonInfo *polygon_info,const double mid,
             }
           else
             {
-              alpha=PerceptibleReciprocal(alpha);
+              alpha=1.0/alpha;
               beta=delta.x*(y-q->y)-delta.y*(x-q->x);
               distance=alpha*beta*beta;
             }
