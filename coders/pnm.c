6f7a9e23d816478c0dcccea32d38cef4ab517019
Cristy <urban-warrior@imagemagick.org>
2022-04-09 12:22:54 -0400

    https://github.com/ImageMagick/ImageMagick/issues/5027

--- coders/pnm.c
+++ coders/pnm.c
@@ -1714,7 +1714,7 @@ static MagickBooleanType WritePNMImage(const ImageInfo *image_info,Image *image)
           {
             packet_size=1;
             (void) CopyMagickString(type,"GRAYSCALE",MaxTextExtent);
-            if (IdentifyImageMonochrome(image,&image->exception) != MagickFalse)
+            if (GetQuantumRange(image->depth) == 1)
               (void) CopyMagickString(type,"BLACKANDWHITE",MaxTextExtent);
             break;
           }
