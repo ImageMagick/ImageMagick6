212564a2693c2e9189d12e11a1d67907ad2cf970
Cristy <urban-warrior@imagemagick.org>
2022-04-14 07:20:20 -0400

    prevent double free in Freetype streaming interface

--- magick/annotate.c
+++ magick/annotate.c
@@ -1566,7 +1566,6 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
           (void) FT_Done_FreeType(library);
           ThrowFreetypeErrorException("UnrecognizedFontEncoding",ft_status,
             encoding);
-          stream=(FT_StreamRec *) RelinquishMagickMemory(stream);
           return(MagickFalse);
         }
     }
@@ -1599,7 +1598,6 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
       (void) FT_Done_FreeType(library);
       ThrowFreetypeErrorException("UnableToReadFont",ft_status,
         draw_info->font);
-      stream=(FT_StreamRec *) RelinquishMagickMemory(stream);
       return(MagickFalse);
     }
   metrics->pixels_per_em.x=face->size->metrics.x_ppem;
@@ -1637,7 +1635,6 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
     {
       (void) FT_Done_Face(face);
       (void) FT_Done_FreeType(library);
-      stream=(FT_StreamRec *) RelinquishMagickMemory(stream);
       return(MagickTrue);
     }
   /*
@@ -1956,7 +1953,6 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
   annotate_info=DestroyDrawInfo(annotate_info);
   (void) FT_Done_Face(face);
   (void) FT_Done_FreeType(library);
-  stream=(FT_StreamRec *) RelinquishMagickMemory(stream);
   return(status);
 }
 #else
