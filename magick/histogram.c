629154d99c33e09c35ed0e2cb147212c5425b6b1
Cristy <urban-warrior@imagemagick.org>
2022-04-16 14:12:15 -0400

    https://github.com/ImageMagick/ImageMagick/issues/4973

--- magick/histogram.c
+++ magick/histogram.c
@@ -1273,7 +1273,7 @@ MagickExport size_t GetNumberColors(const Image *image,FILE *file,
     (void) ConcatenateMagickString(tuple,")",MaxTextExtent);
     (void) QueryMagickColorname(image,&pixel,SVGCompliance,color,exception);
     GetColorTuple(&pixel,MagickTrue,hex);
-    (void) sprintf(count,"%.20g:",(double) ((MagickOffsetType) p->count));
+    (void) sprintf(count,"%10.20g:",(double) ((MagickOffsetType) p->count));
     (void) FormatLocaleFile(file,"    %s %s %s %s\n",count,tuple,hex,color);
     if (image->progress_monitor != (MagickProgressMonitor) NULL)
       {
