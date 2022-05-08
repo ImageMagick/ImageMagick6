679e9267ba039139a37fc36b933b34f2a7b346ce
Loïc Bartoletti <lbartoletti@users.noreply.github.com>
2022-05-08 20:43:48 +0200

    Fix build with -Werror (#177)

--- magick/log.h
+++ magick/log.h
@@ -66,14 +66,14 @@ typedef void
 extern MagickExport char
   **GetLogList(const char *,size_t *,ExceptionInfo *);
 
-extern MagickExport const char
+extern MagickExport char
   *GetLogName(void) magick_attribute((__pure__)),
   *SetLogName(const char *);
 
-extern MagickExport const LogEventType
+extern MagickExport LogEventType
   GetLogEventMask(void) magick_attribute((__pure__));
 
-extern MagickExport const LogInfo
+extern MagickExport LogInfo
   **GetLogInfoList(const char *,size_t *,ExceptionInfo *);
 
 extern MagickExport LogEventType
