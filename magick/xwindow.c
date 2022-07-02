7615041cb424617643406fbf0a7040af541c1b12
Cristy <urban-warrior@imagemagick.org>
2022-07-02 11:35:42 -0400

    eliminate compiler warning

--- magick/xwindow.c
+++ magick/xwindow.c
@@ -5382,6 +5382,7 @@ MagickExport MagickBooleanType XMakeImage(Display *display,
   assert(window != (XWindowInfo *) NULL);
   assert(width != 0);
   assert(height != 0);
+  magick_unreferenced(length);
   if (IsEventLogging() != MagickFalse)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"...");
   if ((window->width == 0) || (window->height == 0))
