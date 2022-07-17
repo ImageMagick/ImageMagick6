dcb6e1ecacf99993d649cf97d7a29ef876ed3a1d
Cristy <urban-warrior@imagemagick.org>
2022-07-17 14:44:56 -0400

    eliminate a double free per https://oss-fuzz.com/testcase-detail/5295466559963136; we're looking for a more robust fix

--- magick/blob.c
+++ magick/blob.c
@@ -2026,13 +2026,14 @@ MagickExport unsigned char *ImagesToBlob(const ImageInfo *image_info,
           status=WriteImages(clone_info,images,images->filename,exception);
           *length=images->blob->length;
           blob=DetachBlob(images->blob);
-          if (blob == (unsigned char *) NULL)
-            clone_info->blob=RelinquishMagickMemory(clone_info->blob);
-          else if (status == MagickFalse)
-            blob=(unsigned char *) RelinquishMagickMemory(blob);
-          else
-            blob=(unsigned char *) ResizeQuantumMemory(blob,*length+1,
-              sizeof(*blob));
+          if (blob != (unsigned char *) NULL)
+            {
+              if (status == MagickFalse)
+                blob=(unsigned char *) RelinquishMagickMemory(blob);
+              else
+                blob=(unsigned char *) ResizeQuantumMemory(blob,*length+1,
+                  sizeof(*blob));
+            }
         }
     }
   else
