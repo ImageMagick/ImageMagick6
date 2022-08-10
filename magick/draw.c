d12288f6ec12343647e5a69ce1ded53241322a2d
Cristy <urban-warrior@imagemagick.org>
2022-08-10 17:28:47 -0400

    a zero length line segment is not a point

--- magick/draw.c
+++ magick/draw.c
@@ -6453,13 +6453,6 @@ static MagickBooleanType TraceLine(PrimitiveInfo *primitive_info,
 {
   if (TracePoint(primitive_info,start) == MagickFalse)
     return(MagickFalse);
-  if ((fabs(start.x-end.x) < MagickEpsilon) &&
-      (fabs(start.y-end.y) < MagickEpsilon))
-    {
-      primitive_info->primitive=PointPrimitive;
-      primitive_info->coordinates=1;
-      return(MagickTrue);
-    }
   if (TracePoint(primitive_info+1,end) == MagickFalse)
     return(MagickFalse);
   (primitive_info+1)->primitive=primitive_info->primitive;
