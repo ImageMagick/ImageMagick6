689288c0653f68f85c74eaddac65bda55d12f736
Cristy <urban-warrior@imagemagick.org>
2022-10-22 11:29:34 -0400

    eliminate unnecessary file open when globbing (thanks to P Antoine)

--- magick/token.c
+++ magick/token.c
@@ -358,12 +358,12 @@ MagickExport MagickBooleanType GlobExpression(
   const char *magick_restrict expression,const char *magick_restrict pattern,
   const MagickBooleanType case_insensitive)
 {
+  char
+    path[MagickPathExtent];
+
   MagickBooleanType
     done,
     match;
-
-  const char
-    *magick_restrict p;
 
   /*
     Return on empty pattern or '*'.
@@ -374,30 +374,9 @@ MagickExport MagickBooleanType GlobExpression(
     return(MagickTrue);
   if (LocaleCompare(pattern,"*") == 0)
     return(MagickTrue);
-  p=pattern+strlen(pattern)-1;
-  if ((GetUTFCode(p) == ']') && (strchr(pattern,'[') != (char *) NULL))
-    {
-      ExceptionInfo
-        *exception;
-
-      ImageInfo
-        *image_info;
-
-      /*
-        Determine if pattern is a scene, i.e. img0001.pcd[2].
-      */
-      image_info=AcquireImageInfo();
-      (void) CopyMagickString(image_info->filename,pattern,MaxTextExtent);
-      exception=AcquireExceptionInfo();
-      (void) SetImageInfo(image_info,0,exception);
-      exception=DestroyExceptionInfo(exception);
-      if (LocaleCompare(image_info->filename,pattern) != 0)
-        {
-          image_info=DestroyImageInfo(image_info);
-          return(MagickFalse);
-        }
-      image_info=DestroyImageInfo(image_info);
-    }
+  GetPathComponent(pattern,SubimagePath,path);
+  if (*path != '\0')
+    return(MagickFalse);
   /*
     Evaluate glob expression.
   */
