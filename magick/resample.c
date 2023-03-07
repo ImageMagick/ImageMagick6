ImageMagick/ImageMagick6

commit f560fc2444497f62c9b50cf6314f31fd148b381e

check for negative LUT lookup (https://github.com/ImageMagick/ImageMagick/issues/6070)

magick/resample.c
@@ -604,14 +604,14 @@ MagickExport MagickBooleanType ResamplePixelColor(
 
     /* count up the weighted pixel colors */
     for( u=0; u<uw; u++ ) {
-      weight = 0;
+      weight = 0.0;
 #if FILTER_LUT
       /* Note that the ellipse has been pre-scaled so F = WLUT_WIDTH */
-      if ( Q < (double)WLUT_WIDTH ) {
+      if ((Q >= 0.0) && ((int) Q < WLUT_WIDTH))
         weight = resample_filter->filter_lut[(int)Q];
 #else
       /* Note that the ellipse has been pre-scaled so F = support^2 */
-      if ( Q < (double)resample_filter->F ) {
+      if ((Q >= 0.0) && (Q < (double)resample_filter->F))
         weight = GetResizeFilterWeight(resample_filter->filter_def,
              sqrt(Q));    /* a SquareRoot!  Arrggghhhhh... */
 #endif

commit f446c564713566141fe40efb2c80705412ea0142

Restore the missing bracket.
This build regression was introduced in
f560fc2444497f62c9b50cf6314f31fd148b381e.

magick/resample.c
@@ -611,7 +611,7 @@ MagickExport MagickBooleanType ResamplePixelColor(
         weight = resample_filter->filter_lut[(int)Q];
 #else
       /* Note that the ellipse has been pre-scaled so F = support^2 */
-      if ((Q >= 0.0) && (Q < (double)resample_filter->F))
+      if ((Q >= 0.0) && (Q < (double)resample_filter->F)) {
         weight = GetResizeFilterWeight(resample_filter->filter_def,
              sqrt(Q));    /* a SquareRoot!  Arrggghhhhh... */
 #endif