c2a977370e4ee5b47cbdb9802ab723689de24568
Cristy <urban-warrior@imagemagick.org>
2023-03-18 22:33:01 -0400

    optimization

--- coders/png.c
+++ coders/png.c
@@ -2220,7 +2220,7 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
     *ping_pixels;
 
 #ifdef PNG_UNKNOWN_CHUNKS_SUPPORTED
-  png_byte unused_chunks[]=
+  static const png_byte unused_chunks[]=
   {
     104,  73,  83,  84, (png_byte) '\0',   /* hIST */
     105,  84,  88, 116, (png_byte) '\0',   /* iTXt */
