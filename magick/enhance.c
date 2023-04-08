3afc67e742807fa80c37f7989eccc9c90408bfa7
Cristy <urban-warrior@imagemagick.org>
2023-04-08 15:53:48 -0400

    correct intercept

--- magick/enhance.c
+++ magick/enhance.c
@@ -301,7 +301,7 @@ MagickExport MagickBooleanType BrightnessContrastImageChannel(Image *image,
   slope=100.0*PerceptibleReciprocal(100.0-contrast);
   if (contrast < 0.0)
     slope=0.01*contrast+1.0;
-  intercept=(0.01*brightness-0.5)+slope+0.5;
+  intercept=(0.01*brightness-0.5)*slope+0.5;
   coefficients[0]=slope;
   coefficients[1]=intercept;
   status=FunctionImageChannel(image,channel,PolynomialFunction,2,coefficients,
