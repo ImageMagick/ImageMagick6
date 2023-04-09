cc11b8baba2ddfe4563a6acc6ee87fc327080c6a
Cristy <urban-warrior@imagemagick.org>
2023-04-09 10:53:11 -0400

    if the image type is explicit, use the file extension if possible (https://github.com/ImageMagick/ImageMagick/issues/6242)

--- magick/image.c
+++ magick/image.c
@@ -2914,6 +2914,9 @@ MagickExport MagickBooleanType SetImageInfo(ImageInfo *image_info,
     {
       (void) CopyMagickString(magic,image_info->magick,MaxTextExtent);
       magick_info=GetMagickInfo(magic,sans_exception);
+      if ((magick_info != (const MagickInfo *) NULL) &&
+          (magick_info->format_type == ExplicitFormatType))
+        image_info->affirm=MagickTrue;
       if (frames == 0)
         GetPathComponent(image_info->filename,CanonicalPath,filename);
       else
