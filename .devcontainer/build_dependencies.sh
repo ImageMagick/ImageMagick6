068a78ce96890a4d09b9d1f817fd809b08462712
Dirk Lemstra <dirk@lemstra.org>
2023-07-13 22:27:40 +0200

    Change the default devcontainer to a security container.

--- .devcontainer/build_dependencies.sh
+++ .devcontainer/build_dependencies.sh
@@ -2,29 +2,29 @@
 
 # build zlib
 pushd "$SRC/zlib"
-./configure --prefix="$WORK"
-make -j$(nproc)
+./configure --static --prefix="$WORK"
+make -j$(nproc) CFLAGS="$CFLAGS -fPIC"
 make install
 popd
 
 # Build xz
 pushd "$SRC/xz"
-./autogen.sh --no-po4a
-./configure --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --disable-shared --with-pic=yes --prefix="$WORK"
+./autogen.sh --no-po4a --no-doxygen
+./configure --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-ifunc --disable-scripts --disable-doc --disable-shared --with-pic=yes --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
 # Build png
 pushd "$SRC/libpng"
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK
+cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DPNG_SHARED=off
 make -j$(nproc)
 make install
 popd
 
 # Build libjpeg-turbo
 pushd "$SRC/libjpeg-turbo"
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK
+CFLAGS="$CFLAGS -fPIC" cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DENABLE_STATIC=on -DENABLE_SHARED=off
 make -j$(nproc)
 make install
 popd
@@ -32,7 +32,7 @@ popd
 # Build libtiff
 pushd "$SRC/libtiff"
 autoreconf -fiv
-./configure --prefix="$WORK"
+./configure --disable-shared --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
@@ -40,7 +40,7 @@ popd
 # Build liblcms2
 pushd "$SRC/Little-CMS"
 autoreconf -fiv
-./configure --prefix="$WORK"
+./configure --disable-shared --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
@@ -48,7 +48,7 @@ popd
 # build libraw
 pushd "$SRC/libraw"
 autoreconf -fiv
-./configure --prefix="$WORK" --with-pic=yes --disable-examples PKG_CONFIG_PATH="$WORK/lib/pkgconfig" CXXFLAGS="-DLIBRAW_USE_CALLOC_INSTEAD_OF_MALLOC=on"
+./configure --prefix="$WORK" --disable-shared --with-pic=yes --disable-examples PKG_CONFIG_PATH="$WORK/lib/pkgconfig" CXXFLAGS="$CXXFLAGS -DLIBRAW_USE_CALLOC_INSTEAD_OF_MALLOC=on"
 make -j$(nproc)
 make install
 popd
@@ -56,7 +56,7 @@ popd
 # Build freetype2
 pushd "$SRC/freetype"
 ./autogen.sh
-./configure --prefix="$WORK" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
+./configure --prefix="$WORK" --disable-shared PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
 make -j$(nproc)
 make install
 popd
@@ -64,15 +64,14 @@ popd
 # Build libde265
 pushd "$SRC/libde265"
 ./autogen.sh
-./configure --prefix="$WORK"
+./configure --disable-shared --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
 # Build libheif
 pushd "$SRC/libheif"
-./autogen.sh
-./configure --disable-go --disable-examples --disable-tests --prefix="$WORK" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
+cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_SHARED_LIBS=off -DBUILD_TESTING=off -DWITH_EXAMPLES=off -DENABLE_PLUGIN_LOADING=off -DWITH_JPEG_DECODER=off -DWITH_JPEG_ENCODER=off -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
 popd
@@ -80,14 +79,14 @@ popd
 # Build webp
 pushd "$SRC/libwebp"
 ./autogen.sh
-./configure --disable-png --disable-jpeg --disable-tiff --prefix="$WORK"
+./configure --disable-shared  --disable-png --disable-jpeg --disable-tiff --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
 # Build openjpg
 pushd "$SRC/openjpeg"
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_CODEC=off -DCMAKE_BUILD_TYPE=Release
+cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_SHARED_LIBS=off -DBUILD_CODEC=off -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
 popd
--- .devcontainer/build_imagemagick.sh
+++ .devcontainer/build_imagemagick.sh
@@ -4,3 +4,18 @@ autoreconf -fiv
 ./configure --prefix="$WORK" --disable-docs CFLAGS="-I$WORK/include" LIBS="-L$WORK/lib" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
 make "-j$(nproc)"
 make install
+
+cat <<EOT >> /ImageMagick/etc/ImageMagick-6/policy.xml
+<policymap>
+  <policy domain="resource" name="memory" value="256MiB"/>
+  <policy domain="resource" name="list-length" value="32"/>
+  <policy domain="resource" name="width" value="8KP"/>
+  <policy domain="resource" name="height" value="8KP"/>
+  <policy domain="resource" name="map" value="512MiB"/>
+  <policy domain="resource" name="area" value="16KP"/>
+  <policy domain="resource" name="disk" value="1GiB"/>
+  <policy domain="resource" name="file" value="768"/>
+  <policy domain="resource" name="thread" value="2"/>
+  <policy domain="resource" name="time" value="120"/>
+</policymap>
+EOT
