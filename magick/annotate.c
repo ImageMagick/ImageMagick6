6342c585c9425f223cddda0c169cf96a94668ec8
Cristy <urban-warrior@imagemagick.org>
2023-07-17 20:02:31 -0400

    note y_advance, implement top-to-bottom in the future

--- magick/annotate.c
+++ magick/annotate.c
@@ -114,7 +114,8 @@ typedef struct _GraphemeInfo
     index,
     x_offset,
     x_advance,
-    y_offset;
+    y_offset,
+    y_advance;
 
   size_t
     cluster;
@@ -1128,11 +1129,11 @@ static size_t ComplexRaqmTextLayout(const Image *image,
   raqm_glyph_t
     *glyphs;
 
-  ssize_t
-    i;
-
   size_t
     extent;
+
+  ssize_t
+    i;
 
   extent=0;
   rq=raqm_create();
@@ -1193,6 +1194,7 @@ static size_t ComplexRaqmTextLayout(const Image *image,
     (*grapheme)[i].x_offset=glyphs[i].x_offset;
     (*grapheme)[i].x_advance=glyphs[i].x_advance;
     (*grapheme)[i].y_offset=glyphs[i].y_offset;
+    (*grapheme)[i].y_advance=glyphs[i].y_advance;
     (*grapheme)[i].cluster=glyphs[i].cluster;
   }
 
