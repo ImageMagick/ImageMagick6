f11ed36c0a14ee1117dcc3542380de83918e138d
Cristy <urban-warrior@imagemagick.org>
2023-10-13 20:42:30 -0400

    cosmetic

--- magick/cache.c
+++ magick/cache.c
@@ -1848,15 +1848,12 @@ static Cache GetImagePixelCache(Image *image,const MagickBooleanType clone,
 
   MagickBooleanType
     destroy,
-    status;
+    status = MagickTrue;
 
   static MagickSizeType
     cpu_throttle = MagickResourceInfinity,
     cycles = 0;
 
-  status=MagickTrue;
-  if (cpu_throttle == MagickResourceInfinity)
-    cpu_throttle=GetMagickResourceLimit(ThrottleResource);
   if (((double) image->timestamp+image->ttl) < (double) GetMagickTime())
     {
       cache_info=(CacheInfo *) image->cache;
@@ -1866,6 +1863,8 @@ static Cache GetImagePixelCache(Image *image,const MagickBooleanType clone,
         ResourceLimitFatalError,"TimeLimitExceeded","`%s'",image->filename);
       return((Cache) NULL);
     }
+  if (cpu_throttle == MagickResourceInfinity)
+    cpu_throttle=GetMagickResourceLimit(ThrottleResource);
   if ((cpu_throttle != 0) && ((cycles++ % 4096) == 0))
     MagickDelay(cpu_throttle);
   LockSemaphoreInfo(image->semaphore);
