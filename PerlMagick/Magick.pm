4712685794628caa7fa61cc2a5063b6879f6d55a
Cristy <urban-warrior@imagemagick.org>
2023-12-03 12:21:37 -0500

    release

--- PerlMagick/Magick.pm
+++ PerlMagick/Magick.pm
@@ -43,7 +43,7 @@ require AutoLoader;
       ConfigureError FatalErrorException
     );
 
-$VERSION = '6.9.12';
+$VERSION = '6.9.13';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
