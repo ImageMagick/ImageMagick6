6746dfffed7821c56ac4e0d3e1cfd265f71a58da
Cristy <urban-warrior@imagemagick.org>
2024-02-10 12:43:32 -0500

    if resampling fails, replace with invalid pixel

--- magick/distort.c
+++ magick/distort.c
@@ -2779,14 +2779,19 @@ if ( d.x == 0.5 && d.y == 0.5 ) {
         }
         else {
           /* resample the source image to find its correct color */
-          (void) ResamplePixelColor(resample_filter[id],s.x,s.y,&pixel);
-          /* if validity between 0.0 and 1.0 mix result with invalid pixel */
-          if ( validity < 1.0 ) {
-            /* Do a blend of sample color and invalid pixel */
-            /* should this be a 'Blend', or an 'Over' compose */
-            MagickPixelCompositeBlend(&pixel,validity,&invalid,(1.0-validity),
-              &pixel);
-          }
+          status=ResamplePixelColor(resample_filter[id],s.x,s.y,&pixel);
+          if (status == MagickFalse)
+            SetPixelPacket(distort_image,&invalid,q,indexes);
+          else
+            {
+              /* if validity between 0.0 & 1.0 mix result with invalid pixel */
+              if ( validity < 1.0 ) {
+                /* Do a blend of sample color and invalid pixel */
+                /* should this be a 'Blend', or an 'Over' compose */
+                MagickPixelCompositeBlend(&pixel,validity,&invalid,(1.0-validity),
+                  &pixel);
+              }
+            }
           SetPixelPacket(distort_image,&pixel,q,indexes);
         }
         q++;
