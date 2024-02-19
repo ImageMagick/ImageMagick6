3da057d31756970cb50b1567922e9caf94d36d84
Cristy <urban-warrior@imagemagick.org>
2024-02-18 19:05:34 -0500

    adjust browser spacing between buttons

--- magick/widget.c
+++ magick/widget.c
@@ -1792,8 +1792,8 @@ MagickExport void XColorBrowserWidget(Display *display,XWindows *windows,
         XGetWidgetInfo(action,&action_info);
         action_info.width=width;
         action_info.height=(unsigned int) ((3*height) >> 1);
-        action_info.x=cancel_info.x-(cancel_info.width+(QuantumMargin >> 1)+
-          (action_info.bevel_width << 1));
+        action_info.x=(int) windows->widget.width-(int) action_info.width-
+          (int) cancel_info.width-2*QuantumMargin-2;
         action_info.y=cancel_info.y;
         XGetWidgetInfo(GrabButtonText,&grab_info);
         grab_info.width=width;
@@ -5668,8 +5668,8 @@ MagickExport void XFontBrowserWidget(Display *display,XWindows *windows,
         XGetWidgetInfo(action,&action_info);
         action_info.width=width;
         action_info.height=(unsigned int) ((3*height) >> 1);
-        action_info.x=cancel_info.x-(cancel_info.width+(QuantumMargin >> 1)+
-          (action_info.bevel_width << 1));
+        action_info.x=(int) windows->widget.width-(int) action_info.width-
+          (int) cancel_info.width-2*QuantumMargin-2;
         action_info.y=cancel_info.y;
         XGetWidgetInfo(BackButtonText,&back_info);
         back_info.width=width;
