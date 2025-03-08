60f2502ae79fea05f8adf57693df2d0b9ed49825
Cristy <urban-warrior@imagemagick.org>
2025-03-08 09:14:57 -0500

    detect invalid compression BMP

--- coders/bmp.c
+++ coders/bmp.c
@@ -835,6 +835,8 @@ static Image *ReadBMPImage(const ImageInfo *image_info,ExceptionInfo *exception)
             (void) LogMagickEvent(CoderEvent,GetMagickModule(),
               "  Number of colors: %u",bmp_info.number_colors);
           }
+        if ((bmp_info.height < 0) && (bmp_info.compression != 0))
+          ThrowReaderException(CoderError,"CompressNotSupported");
         if ((bmp_info.size > 40) || (bmp_info.compression == BI_BITFIELDS) ||
             (bmp_info.compression == BI_ALPHABITFIELDS))
           {
