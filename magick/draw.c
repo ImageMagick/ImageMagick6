5a29c566afb4cdfc01eb005c2cfb29221871f8be
Cristy <urban-warrior@imagemagick.org>
2025-03-29 13:12:12 -0400

    render closed sub-path poly lines

--- magick/draw.c
+++ magick/draw.c
@@ -7317,9 +7317,9 @@ static PrimitiveInfo *TraceStrokePolygon(const DrawInfo *draw_info,
   offset.x=primitive_info[number_vertices-1].point.x-primitive_info[0].point.x;
   offset.y=primitive_info[number_vertices-1].point.y-primitive_info[0].point.y;
   closed_path=(fabs(offset.x) < MagickEpsilon) &&
-    (fabs(offset.y) < MagickEpsilon) ?  MagickTrue : MagickFalse;
-  if (((draw_info->linejoin == RoundJoin) ||
-       (draw_info->linejoin == MiterJoin)) && (closed_path != MagickFalse))
+    (fabs(offset.y) < MagickEpsilon) ? MagickTrue : MagickFalse;
+  if ((draw_info->linejoin == MiterJoin) ||
+      ((draw_info->linejoin == RoundJoin) && (closed_path != MagickFalse)))
     {
       polygon_primitive[number_vertices]=primitive_info[1];
       number_vertices++;
@@ -7328,6 +7328,7 @@ static PrimitiveInfo *TraceStrokePolygon(const DrawInfo *draw_info,
   /*
     Compute the slope for the first line segment, p.
   */
+  closed_path=primitive_info[0].closed_subpath;
   dx.p=0.0;
   dy.p=0.0;
   for (n=1; n < (ssize_t) number_vertices; n++)
