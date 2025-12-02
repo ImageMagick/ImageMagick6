7779f1ff772dfabe545c67fb2f3bfa8f7a845a2d
Dirk Lemstra <dirk@lemstra.org>
2025-12-02 22:49:19 +0100

    Added extra check to avoid an overflow on 32-bit machines (GHSA-6hjr-v6g4-3fm8)

--- coders/tim.c
+++ coders/tim.c
@@ -234,7 +234,10 @@ static Image *ReadTIMImage(const ImageInfo *image_info,ExceptionInfo *exception)
     (void) ReadBlobLSBShort(image);
     width=ReadBlobLSBShort(image);
     height=ReadBlobLSBShort(image);
-    image_size=2*width*height;
+    if (HeapOverflowSanityCheckGetSize(2*width,height,&image_size) != MagickFalse)
+      ThrowReaderException(CorruptImageError,"ImproperImageHeader");
+    if (image_size > GetBlobSize(image))
+      ThrowReaderException(CorruptImageError,"InsufficientImageDataInFile");
     bytes_per_line=width*2;
     width=(width*16)/bits_per_pixel;
     image->columns=width;
