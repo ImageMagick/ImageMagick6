38872ec2a70084813883ea152f18497911823c18
Dirk Lemstra <dirk@lemstra.org>
2026-01-25 19:35:35 +0100

    Initialize the pixels with empty values to prevent possible heap information disclosure (https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-96pc-27rx-pr36)

--- coders/psd.c
+++ coders/psd.c
@@ -1261,6 +1261,7 @@ static MagickBooleanType ReadPSDChannelZip(Image *image,const size_t channels,
       ThrowBinaryException(ResourceLimitError,"MemoryAllocationFailed",
         image->filename);
     }
+  memset(pixels,0,count*sizeof(*pixels));
   if (ReadBlob(image,compact_size,compact_pixels) != (ssize_t) compact_size)
     {
       pixels=(unsigned char *) RelinquishMagickMemory(pixels);
