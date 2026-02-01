4c78305165c97451b1e1534727c5aa8da31f3be3
Cristy <urban-warrior@imagemagick.org>
2026-02-01 15:02:55 -0500

    https://github.com/ImageMagick/ImageMagick/issues/8552

--- coders/wpg.c
+++ coders/wpg.c
@@ -1189,7 +1189,7 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
               WPG_Palette.StartIndex=ReadBlobLSBShort(image);
               WPG_Palette.NumOfEntries=ReadBlobLSBShort(image);
               if ((WPG_Palette.NumOfEntries-WPG_Palette.StartIndex) >
-                  (Rec2.RecordLength-2-2) / 3)
+                  (Rec.RecordLength-2-2) / 3)
                 ThrowReaderException(CorruptImageError,"InvalidColormapIndex");
               if (WPG_Palette.StartIndex > WPG_Palette.NumOfEntries)
                 ThrowReaderException(CorruptImageError,"InvalidColormapIndex");
