9dd1ce6d8c1f66971cef275fb31cc079b9f4e186
Cristy <urban-warrior@imagemagick.org>
2026-02-01 13:55:31 -0500

    https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-xg29-8ghv-v4xr

--- coders/sixel.c
+++ coders/sixel.c
@@ -237,8 +237,8 @@ MagickBooleanType sixel_decode(Image *image,
                                unsigned char              /* out */ **palette,  /* ARGB palette */
                                size_t                     /* out */ *ncolors    /* palette size (<= 256) */)
 {
-    int n, i, r, g, b, sixel_vertical_mask, c;
-    int posision_x, posision_y;
+    int n, r, g, b, sixel_vertical_mask, c;
+    int position_x, position_y;
     int max_x, max_y;
     int attributed_pan, attributed_pad;
     int attributed_ph, attributed_pv;
@@ -246,13 +246,12 @@ MagickBooleanType sixel_decode(Image *image,
     int param[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     int sixel_palet[SIXEL_PALETTE_MAX];
     unsigned char *imbuf, *dmbuf;
-    int y;
-    size_t extent,offset;
-    ssize_t imsx, imsy;
+    size_t extent;
+    ssize_t i, imsx, imsy, offset, y;
     ssize_t dmsx, dmsy;
 
     extent=strlen((char *) p);
-    posision_x = posision_y = 0;
+    position_x = position_y = 0;
     max_x = max_y = 0;
     attributed_pan = 2;
     attributed_pad = 1;
@@ -422,22 +421,22 @@ MagickBooleanType sixel_decode(Image *image,
         } else if (*p == '$') {
             /* DECGCR Graphics Carriage Return */
             p++;
-            posision_x = 0;
+            position_x = 0;
             repeat_count = 1;
 
         } else if (*p == '-') {
             /* DECGNL Graphics Next Line */
             p++;
-            posision_x  = 0;
-            posision_y += 6;
+            position_x  = 0;
+            position_y += 6;
             repeat_count = 1;
 
         } else if (*p >= '?' && *p <= '\177') {
-            if (imsx < (posision_x + repeat_count) || imsy < (posision_y + 6)) {
+            if (imsx < ((ssize_t) position_x+repeat_count) || imsy < ((ssize_t) position_y+6)) {
                 ssize_t nx = imsx * 2;
                 ssize_t ny = imsy * 2;
 
-                while (nx < (posision_x + repeat_count) || ny < (posision_y + 6)) {
+                while (nx < ((ssize_t) position_x+repeat_count) || ny < ((ssize_t) position_y+6)) {
                     nx *= 2;
                     ny *= 2;
                 }
@@ -465,7 +464,7 @@ MagickBooleanType sixel_decode(Image *image,
                 max_color_index = color_index;
             }
             if ((b = *(p++) - '?') == 0) {
-                posision_x += repeat_count;
+                position_x += repeat_count;
 
             } else {
                 sixel_vertical_mask = 0x01;
@@ -473,23 +472,23 @@ MagickBooleanType sixel_decode(Image *image,
                 if (repeat_count <= 1) {
                     for (i = 0; i < 6; i++) {
                         if ((b & sixel_vertical_mask) != 0) {
-                            offset=(size_t) imsx * (posision_y + i) + posision_x;
-                            if (offset >= (size_t) imsx * imsy)
+                            offset=imsx*(position_y+i)+position_x;
+                            if (offset >= (imsx*imsy))
                               {
                                 imbuf = (unsigned char *) RelinquishMagickMemory(imbuf);
                                 return (MagickFalse);
                               }
                             imbuf[offset] = color_index;
-                            if (max_x < posision_x) {
-                                max_x = posision_x;
+                            if (max_x < position_x) {
+                                max_x = position_x;
                             }
-                            if (max_y < (posision_y + i)) {
-                                max_y = posision_y + i;
+                            if (max_y < (position_y + i)) {
+                                max_y = position_y + i;
                             }
                         }
                         sixel_vertical_mask <<= 1;
                     }
-                    posision_x += 1;
+                    position_x += 1;
 
                 } else { /* repeat_count > 1 */
                     for (i = 0; i < 6; i++) {
@@ -501,20 +500,20 @@ MagickBooleanType sixel_decode(Image *image,
                                 }
                                 c <<= 1;
                             }
-                            for (y = posision_y + i; y < posision_y + i + n; ++y) {
-                                offset=(size_t) imsx * y + posision_x;
-                                if (offset + repeat_count >= (size_t) imsx * imsy)
+                            for (y = position_y + i; y < position_y + i + n; ++y) {
+                                offset=imsx*y+position_x;
+                                if ((offset+repeat_count) >= (imsx*imsy))
                                   {
                                     imbuf = (unsigned char *) RelinquishMagickMemory(imbuf);
                                     return (MagickFalse);
                                   }
                                 (void) memset(imbuf + offset, color_index, repeat_count);
                             }
-                            if (max_x < (posision_x + repeat_count - 1)) {
-                                max_x = posision_x + repeat_count - 1;
+                            if (max_x < (position_x + repeat_count - 1)) {
+                                max_x = position_x + repeat_count - 1;
                             }
-                            if (max_y < (posision_y + i + n - 1)) {
-                                max_y = posision_y + i + n - 1;
+                            if (max_y < (position_y + i + n - 1)) {
+                                max_y = position_y + i + n - 1;
                             }
 
                             i += (n - 1);
@@ -522,7 +521,7 @@ MagickBooleanType sixel_decode(Image *image,
                         }
                         sixel_vertical_mask <<= 1;
                     }
-                    posision_x += repeat_count;
+                    position_x += repeat_count;
                 }
             }
             repeat_count = 1;
