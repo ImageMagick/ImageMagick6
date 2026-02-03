9d2c692d2271b2933654b4fc2892316b68d4c64b
Cristy <urban-warrior@imagemagick.org>
2026-02-02 20:08:44 -0500

    https://github.com/ImageMagick/ImageMagick/issues/8553

--- coders/meta.c
+++ coders/meta.c
@@ -824,7 +824,7 @@ static ssize_t parse8BIMW(Image *ifile, Image *ofile)
 
                     next=0;
                     outputlen += len;
-                    while (len--)
+                    while (len-- > 0)
                       (void) WriteBlobByte(ofile,(unsigned char) token[next++]);
 
                     if (outputlen & 1)
@@ -858,7 +858,7 @@ static ssize_t parse8BIMW(Image *ifile, Image *ofile)
                     outputlen += 5;
                     next=0;
                     outputlen += len;
-                    while (len--)
+                    while (len-- > 0)
                       (void) WriteBlobByte(ofile,(unsigned char) token[next++]);
                   }
               }
