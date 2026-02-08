abfbcfe8e7884deb3560c74569c96ee4b068f3a6
Dirk Lemstra <dirk@lemstra.org>
2026-02-08 14:15:18 +0100

    Fixed out of bound read with negative pixel index (https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-vpxv-r9pg-7gpr)

--- coders/xpm.c
+++ coders/xpm.c
@@ -931,10 +931,14 @@ static MagickBooleanType WritePICONImage(const ImageInfo *image_info,
     for (x=0; x < (ssize_t) picon->columns; x++)
     {
       k=((ssize_t) GetPixelIndex(indexes+x) % MaxCixels);
+      if (k < 0)
+        k=0;
       symbol[0]=Cixel[k];
       for (j=1; j < (ssize_t) characters_per_pixel; j++)
       {
         k=(((int) GetPixelIndex(indexes+x)-k)/MaxCixels) % MaxCixels;
+        if (k < 0)
+          k=0;
         symbol[j]=Cixel[k];
       }
       symbol[j]='\0';
