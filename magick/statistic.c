6ef5795c45b9c7f2e0916bc72ddd55bc041ec564
Cristy <urban-warrior@imagemagick.org>
2026-05-31 10:48:16 -0400

    https://github.com/ImageMagick/ImageMagick/issues/8766

--- magick/statistic.c
+++ magick/statistic.c
@@ -2194,32 +2194,9 @@ MagickExport ChannelPerceptualHash *GetImageChannelPerceptualHash(
       return((ChannelPerceptualHash *) NULL);
     }
   for (channel=0; channel <= CompositeChannels; channel++)
-  {
-    double
-      mn = DBL_MAX,
-      mx = -DBL_MAX;
-
     for (i=0; i < MaximumNumberOfImageMoments; i++)
-    {
-      double x = moments[channel].I[i];
-      double alpha = (x > 0.0 ? 1.0 : (x < 0.0 ? -1.0 : 0.0))*
-        log10(fabs(x)+MagickEpsilon);
-
-      if (alpha < mn)
-        mn=alpha;
-      if (alpha > mx)
-        mx=alpha;
-    }
-    for (i=0; i < MaximumNumberOfImageMoments; i++)
-    {
-      double x = moments[channel].I[i];
-      double alpha = (x > 0.0 ? 1.0 : (x < 0.0 ? -1.0 : 0.0))*
-        log10(fabs(x)+MagickEpsilon);
-      double scaled = 255.0*(alpha-mn)*MagickSafeReciprocal(mx-mn);
-      perceptual_hash[channel].Q[i]=MagickMin(MagickMax(scaled,0.0),
-        255.0);
-    }
-  }
+      perceptual_hash[channel].Q[i]=(-MagickSafeLog10(fabs(
+        moments[channel].I[i])));
   moments=(ChannelMoments *) RelinquishMagickMemory(moments);
   return(perceptual_hash);
 }
