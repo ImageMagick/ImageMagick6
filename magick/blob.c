9b1eb6842740986e9b9bc482c692ea15311d7984
Cristy <urban-warrior@imagemagick.org>
2026-06-13 13:01:41 -0400

    https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-3pqj-prwm-c34w

--- magick/blob.c
+++ magick/blob.c
@@ -2782,7 +2782,7 @@ MagickExport MagickBooleanType OpenBlob(const ImageInfo *image_info,
       blob_info->exempt=MagickTrue;
       return(SetStreamBuffering(image_info,image));
     }
-  if ((LocaleNCompare(filename,"fd:",3) == 0) &&
+  if ((strncmp(filename,"fd:",3) == 0) &&
       (IsGeometry(filename+3) != MagickFalse))
     {
       char
