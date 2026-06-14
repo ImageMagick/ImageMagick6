ffc96e2a4cdc2fcbb9e0f18f082be52e1b4ca012
Cristy <urban-warrior@imagemagick.org>
2026-06-14 06:56:54 -0400

    https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-vghg-5jrg-2398

--- magick/policy-private.h
+++ magick/policy-private.h
@@ -22,6 +22,8 @@
 extern "C" {
 #endif
 
+#include "magick/utility-private.h"
+
 #if MAGICKCORE_ZERO_CONFIGURATION_SUPPORT
 /*
   Zero configuration security policy.  Discussion @
@@ -36,6 +38,16 @@ static const char
 extern MagickPrivate MagickBooleanType
   IsRightsAuthorizedByName(const PolicyDomain,const char *,const PolicyRights,
     const char *);
+
+static inline MagickBooleanType IsPathAuthorized(const PolicyRights rights,
+  const char *filename)
+{
+  MagickBooleanType status =
+    ((IsRightsAuthorized(PathPolicyDomain,rights,filename) != MagickFalse) &&
+    ((IsRightsAuthorizedByName(SystemPolicyDomain,"symlink",rights,"follow") != MagickFalse) ||
+     (is_symlink_utf8(filename) == MagickFalse)));
+  return(status);
+}
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
