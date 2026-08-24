9625680bf37338a2bc9c0614eadecbd8351241f2
Cristy <urban-warrior@imagemagick.org>
2026-08-24 11:46:28 -0400

    eliminate compiler exception

--- coders/flif.c
+++ coders/flif.c
@@ -513,7 +513,7 @@ static MagickBooleanType WriteFLIFImage(const ImageInfo *image_info,
           flif_image_write_row_RGBA8(flifimage,y,pixels,length);
         }
     }
-    if (image->ticks_er_second != 0)
+    if (image->ticks_per_second != 0)
      flif_image_set_frame_delay(flifimage,(uint32_t) (100*image->delay/
        image->ticks_per_second));
     flif_encoder_add_image(flifenc,flifimage);
