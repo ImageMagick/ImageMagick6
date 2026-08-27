ac85c8e0a319b1eaf4c3d05d326605f5bae723d6
Dirk Lemstra <dirk@lemstra.org>
2026-08-27 20:24:34 +0200

    Fixed the policy check for coders (https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-vcjj-32hg-qpx5)

--- magick/constitute.c
+++ magick/constitute.c
@@ -563,7 +563,7 @@ MagickExport Image *ReadImage(const ImageInfo *image_info,
       thread_support=GetMagickThreadSupport(magick_info);
       if ((thread_support & DecoderThreadSupport) == 0)
         LockSemaphoreInfo(magick_info->semaphore);
-      status=IsCoderAuthorized(magick_info->magick_module,read_info->magick,
+      status=IsCoderAuthorized(magick_info->magick_module,magick_info->name,
         ReadPolicyRights,exception);
       image=(Image *) NULL;
       if (status != MagickFalse)
