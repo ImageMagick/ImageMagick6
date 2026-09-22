55346382cebdf5c5fa6c544a7df7ae4910ffb090
Cristy <urban-warrior@imagemagick.org>
2026-09-22 07:16:41 -0400

    https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-hvwf-wmm6-hvc3

--- coders/png.c
+++ coders/png.c
@@ -9861,12 +9861,18 @@ static MagickBooleanType WriteOnePNGImage(MngInfo *mng_info,
 
 #endif
   if (ping == (png_struct *) NULL)
-    ThrowWriterException(ResourceLimitError,"MemoryAllocationFailed");
+    {
+      image_info=DestroyImageInfo(image_info);
+      image=DestroyImage(image);
+      ThrowWriterException(ResourceLimitError,"MemoryAllocationFailed");
+    }
 
   ping_info=png_create_info_struct(ping);
 
   if (ping_info == (png_info *) NULL)
     {
+      image_info=DestroyImageInfo(image_info);
+      image=DestroyImage(image);
       png_destroy_write_struct(&ping,(png_info **) NULL);
       ThrowWriterException(ResourceLimitError,"MemoryAllocationFailed");
     }
